c52edc81f185fd31b0254e654be1ea23512a45f4
Joshua Oring <josh@j4cap.com>
2024-06-10 23:38:58 -0400

    SCANNER DONE WOO, also major do over to everything

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module dstat
 
 go 1.22
+
+require github.com/spf13/pflag v1.0.5 // indirect
--- main.go
+++ main.go
@@ -1,128 +1,70 @@
 package main
 
 import (
-	"bufio"
 	"dstat/internal/funcs"
-	"dstat/internal/help"
 	"fmt"
+	flag "github.com/spf13/pflag"
 	"os"
-	"slices"
-	"strings"
 )
 
+/*
+	Flag ideas
+
+	-r --round [int]
+	--mean
+	--stdev
+	-var --variance
+	--max
+	--min
+	-q --quartile [1-3]
+	-med --median
+	--stdevp
+	-varp --variance-population
+	--mode
+	-i --ignore string
+	-n --count
+	-s --summary
+	-sc --scientific
+	-v --version
+*/
+
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr,
+			"Usage of %s: %s [OPTIONS]... [FILES]...\nIf FILES is not set read from standard input.\n",
+			os.Args[0], os.Args[0])
+		flag.PrintDefaults()
+	}
 
-	mode := funcs.SAMPLE
+	// TODO: Add help messages
+	var help *bool = flag.BoolP("help", "h", false, "Display this message")
+
+	flag.Parse()
+
+	if flag.NArg()|flag.NFlag() == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	separators := []rune{}
+	ignore := []rune{}
+
 	var data []float64
+	var err error
+	if flag.NArg() == 0 {
 
-Loop:
-	for {
-		fmt.Print("dstat> ")
-
-		reader := bufio.NewReader(os.Stdin)
-		input, err := reader.ReadString('\n')
-
+	} else {
+		data, err = funcs.ReadFiles(flag.Args(), separators, ignore)
 		if err != nil {
-			panic(err)
-		}
-
-		fields := strings.Fields(input)
-
-		if len(fields) == 0 {
-			continue
-		}
-
-		switch fields[0] {
-
-		case "data":
-			tmp := funcs.ScanLineData(fields[1:])
-
-			if len(tmp) == 0 {
-				fmt.Println(help.Data)
-				continue
-			}
-			data = tmp
-			slices.Sort(data)
-
-		case "avg", "mean":
-			if len(data) == 0 {
-				funcs.NoData()
-				continue
-			}
-			fmt.Println(funcs.Mean(data))
-
-		case "med", "median":
-			if len(data) == 0 {
-				funcs.NoData()
-				continue
-			}
-			fmt.Println(funcs.Median(data))
-
-		case "min":
-			if len(data) == 0 {
-				funcs.NoData()
-				continue
-			}
-			fmt.Println(data[0])
-
-		case "max":
-			if len(data) == 0 {
-				funcs.NoData()
-				continue
-			}
-			fmt.Println(data[len(data)-1])
-
-		case "sort":
-			if len(data) == 0 {
-				funcs.NoData()
-				continue
-			}
-			for _, num := range data {
-				fmt.Print(num, " ")
-			}
-			fmt.Print("\n")
-
-		case "population":
-			mode = funcs.POPULATION
-
-		case "sample":
-			mode = funcs.SAMPLE
-
-		case "variance":
-			if len(data) == 0 {
-				funcs.NoData()
-				continue
-			}
-			fmt.Println(funcs.Variance(data, mode))
-
-		case "stdev":
-			if len(data) == 0 {
-				funcs.NoData()
-				continue
-			}
-			fmt.Println(funcs.StandardDeviation(data, mode))
-
-		case "count":
-			fmt.Println(len(data))
-
-		//case "zscore":
-		//	if len(data) == 0 {
-		//		funcs.NoData()
-		//		continue
-		//	}
-		//	//fmt.Println(funcs.ZScore(data, mode))
-
-		case "help":
-			fmt.Println("NOT IMPLEMENTED")
-
-		case "exit":
-			break Loop
-
-		default:
-			_, err := fmt.Fprintf(os.Stderr, "no such command: %s\n", fields[0])
-			if err != nil {
-				panic(err)
-			}
+			fmt.Println(err.Error())
+			os.Exit(1)
 		}
 	}
+	fmt.Println(funcs.Mean(data))
+
 }
